041f0f6a3504c0bac3b7056018b09f19942cdec0
Grant Briggs <gshbriggs@gmail.com>
2018-11-05 22:28:39 -0500

    Add more columns to schemas (#7)
    * Create .gitignore
    
    * Create example Lambda function
    
    * Create .travis.yml
    
    * Delete redundant .gitignore
    
    * Create database migrations scripts
    
    * Add more columns
    
    * added extra linking table
    
    * Use geography instead of POINT and change VARCHAR sizes
    
    * Use constraints when dropping extensions

--- infrastructure/tables/2_create_tables_table.up.sql
+++ infrastructure/tables/2_create_tables_table.up.sql
@@ -1,3 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS postgis;
+
 CREATE TABLE tables (
-  id SERIAL PRIMARY KEY
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  location geography(POINT),
+  address VARCHAR(255),
+  city VARCHAR(255),
+  state VARCHAR(50),
+  zip VARCHAR(10), 
+  phone VARCHAR(15)
 );
--- infrastructure/tables/3_create_posts_table.up.sql
+++ infrastructure/tables/3_create_posts_table.up.sql
@@ -1,3 +1,5 @@
 CREATE TABLE posts (
-  id SERIAL PRIMARY KEY
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  description TEXT
 );
